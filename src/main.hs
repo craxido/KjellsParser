3df93210653de4312d240c3aca7f53cf05eab5a5
Jonatan Källman <craxi@live.se>
2017-11-02 16:09:35 +0100

    Added the sample grammar to parse it with BNFC just to test - commented it out for now.

--- src/main.hs
+++ src/main.hs
@@ -1,2 +1,8 @@
-main :: IO ()
-main = putStrLn "Hello"
+-- SAMPLE GRAMMAR; file Calc.bnfc
+
+--EAdd.	Exp	::=	Exp	"+"	Exp1	;
+--ESub.	Exp	::=	Exp	"-"	Exp1	;
+--EMul.	Exp1	::=	Exp1	"*"	Exp2	;
+--EDiv.	Exp1	::=	Exp1	"/"	Exp2	;
+--EInt.	Exp2	::=	Integer	;
+--coercions	Exp	2	;
